636ef02a49c81ac9421a3bb6f7c551fe7e31017a
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-04-11 22:27:29 -0300

    feat: type conversion

---
+++ type_conversion_example.py
@@ -0,0 +1,15 @@
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - int(birth_year)
+# print(age)
+#
+# int()
+# float()
+# bool()
+# str()
+
+# Exercise
+
+first = float(input("First: "))
+second = float(input("Second: "))
+sum = first + second
+print("Sum: " + str(sum))
