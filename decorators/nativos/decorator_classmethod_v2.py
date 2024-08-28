1de7cfcfd5ebe74579e32c93daf2e012e6c216bd
Eduardo Freitas <56324728+Edufreitass@users.noreply.github.com>
2024-08-28 20:56:39 -0300

    Create decorator_classmethod_v2.py

---
+++ decorators/nativos/decorator_classmethod_v2.py
@@ -0,0 +1,12 @@
+class Utilidades:
+    """
+    @classmethod:
+    Usado para declarar métodos de classe, que recebem a classe como o primeiro argumento, em vez da instância.
+    """
+
+    @classmethod
+    def exemplo(cls):
+        print("Exemplo")
+
+
+Utilidades.exemplo()  # Saída: Exemplo
